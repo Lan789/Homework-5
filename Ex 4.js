12dfe767eaa39c7a442f64317c2530559d00e180
Lan789 <58630706+Lan789@users.noreply.github.com>
2020-01-14 22:11:10 +0400

    Add files via upload

---
+++ Ex 4.js
@@ -0,0 +1,11 @@
+
+//4. Given an array and a number N.  Write a recursive function that rotates an array N places to the left. (Hint: to add element to the beginning use arr.unshift())
+
+
+Array.prototype.rotate = function(n) {
+ while(this.length && n++) {
+   this.unshift(this.pop());
+ }
+ return this;
+}
+console.log(['a','b','c','d','e','f','g','h'].rotate(3))s
